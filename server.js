8f407d0005b0e611a97470df00962e5ff2ad17fb
AnanSer <ananserbesa@gmail.com>
2024-12-18 11:28:33 +0300

    connect the js to the room.ejs

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
-
 const app = express();
-
 const server = require("http").Server(app);
+const { v4: uuidV4 } = require("uuid");
 
+// Set EJS as the template engine
+app.set("view engine", "ejs");
+
+// Serve static files from the "public" folder
+app.use(express.static("public"));
+
+// Redirect to a room with a unique UUID
 app.get("/", (req, res) => {
-  res.send("hi");
+  res.redirect(`/${uuidV4()}`);
 });
 
-server.listen(process.env.PORT || 3000);
+// Render the room page
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
